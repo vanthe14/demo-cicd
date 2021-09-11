2606a140cb90f9b23947e86b6c3ee9981cc63b4b
vanthe14 <vanthe14@gmail.com>
2021-09-11 18:37:35 +0700

    update featureA

--- app/Http/Controllers/FeatureA.php
+++ app/Http/Controllers/FeatureA.php
@@ -7,4 +7,7 @@ use Illuminate\Http\Request;
 class FeatureA extends Controller
 {
     //
+    public function validate(Request $request, array $rules, array $messages = [], array $customAttributes = [])
+    {
+    }
 }
